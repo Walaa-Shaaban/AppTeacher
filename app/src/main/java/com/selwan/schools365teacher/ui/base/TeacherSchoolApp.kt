cb9aa439cec34afdcde5f928efc9321805d3002d
Khalid Kadamani <khalidqadamani@hotmail.com>
2019-09-03 09:54:24 +0300

    initial commeit

---
+++ app/src/main/java/com/selwan/schools365teacher/ui/base/TeacherSchoolApp.kt
@@ -0,0 +1,10 @@
+package com.selwan.schools365teacher.ui.base
+
+import android.app.Application
+
+class TeacherSchoolApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
